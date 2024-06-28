760b10fa32ca7fa04bba3f0f052ef936cd7e5ca7
DilanMaigua <dilanmaigua@hotmail.com>
2024-06-27 21:15:16 -0500

    Cambio en la linea 24

--- AppArrays/src/Main.java
+++ AppArrays/src/Main.java
@@ -23,6 +23,8 @@ public class Main {
         //Hacer doble bucle for/for
         String user = config[XPROD][CUSER];
 
+
+
         //CLAVE: DESARROLLO
 
         //System.out.println(config[3][1]);
